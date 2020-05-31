8ca46a369f83b9172d91996ed11e6e4ad088b2d2
Pramod Verma <verma.the1@gmail.com>
2020-05-31 20:46:33 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 "use strict";
-//require("dotenv").config(); // this file has to removed and env variables are to be kept in heroku
+
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
